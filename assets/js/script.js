8d08dd5a937b49b45b293ab7d9dddfaae7a8de42
Jesuan Goalhath <jesuanprincdd@gmail.com>
2021-12-27 21:19:06 -0300

    Criando a Estrutura HTML E CSS e também a base javascript

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+let currentQuestion = 0;
+
+showQuestion();
+
+function showQuestion() {
+    if(questions[currentQuestion]) {
+        let q = questions[currentQuestion];
+
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+        document.querySelector('.options').innerHTML = '';
+
+        let optionsHtml = '';
+        for(let i in q.options) {
+           optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+    }
+    else {
+        //acabaram a questões
+    }
+}
